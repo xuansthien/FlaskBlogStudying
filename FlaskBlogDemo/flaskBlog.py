1079d1b72ec698d252f0792b2a3bf6edb5689c3b
Xuan Thien <thiensanhxuan90@gmail.com>
2022-07-26 15:53:03 +0700

    Disable Flask SQLALCHEMY

--- FlaskBlogDemo/flaskBlog.py
+++ FlaskBlogDemo/flaskBlog.py
@@ -8,12 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-
+# class User(db.Model):
+#     id = db.Column(db.Interger, primary_key=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 
 
 posts = [
