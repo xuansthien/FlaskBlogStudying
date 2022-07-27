309300bf042dd170c6aee064d961e8452f178ede
Xuan Thien <thiensanhxuan90@gmail.com>
2022-07-27 15:55:13 +0700

    Study on Flask login, logout

--- FlaskBlogDemo/flaskBlog/forms.py
+++ FlaskBlogDemo/flaskBlog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskBlog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,6 +11,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a difference one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a difference one.')
 
 
 class LoginForm(FlaskForm):
