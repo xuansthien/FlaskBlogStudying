84ebaf5226c1eaf25f5a53d4f418fd1b43ec149a
Xuan Thien <thiensanhxuan90@gmail.com>
2022-07-27 17:05:28 +0700

    End_date 27/7/2022 update file

--- FlaskBlogDemo/flaskBlog/forms.py
+++ FlaskBlogDemo/flaskBlog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskBlog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -29,3 +31,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a difference one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a difference one.')
--- FlaskBlogDemo/flaskBlog/routes.py
+++ FlaskBlogDemo/flaskBlog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskBlog import app, db, bcrypt
-from flaskBlog.forms import RegistrationForm, LoginForm
+from flaskBlog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskBlog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -71,4 +71,7 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
